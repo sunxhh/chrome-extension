4d1c6182b4149658d0f9d60efaf2964fc8a58608
sunxh <997854244@qq.com>
2018-08-24 16:59:55 +0800

    思考

--- src/base/directives/scroll.js
+++ src/base/directives/scroll.js
@@ -1,15 +1,27 @@
+import {
+	toTop
+} from '../../common/js/dom';
+let originScrollDom = {};
+
+const bindScroller = function (parent, children) {
+
+}
+const scrollCurrentType = function (data, el) {
+	let scrollTop = el.scrollTop;
+}
+
 const dict = {
 	scroll: function (el, data) {
 		el.addEventListener('scroll', () => {
-
+			originScrollDom = el;
 		}, false);
 	},
-	
+
 }
 
 
 export default function (Vue) {
-	Vue.directive('drag', {
+	Vue.directive('scroll', {
 		// 当被绑定的元素插入到 DOM 中时……
 		inserted: function (el, binding) {
 			let type = binding.arg;
